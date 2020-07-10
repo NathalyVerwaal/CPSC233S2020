cb759bf660fba1189200788fbcc474e0638ab9d7
NathalyVerwaal <47015175+NathalyVerwaal@users.noreply.github.com>
2020-07-10 10:06:40 -0600

    Encapsulated instance variables
    Encapsulated the instance variables and made sure the denominator would never be zero.

--- Fraction.java
+++ Fraction.java
@@ -1,38 +1,56 @@
 import java.math.BigInteger;
 
 public class Fraction {
-	int numerator = 0;
-	int denominator = 1;
+	// class is responsible for making sure that a Fraction object is always
+	// in a valid state.
+	private int numerator = 0;
+	private int denominator = 1;  // this should never be zero.
 	
 	public Fraction(int aNumerator, int aDenominator) {
-		numerator = aNumerator;
+		setNumerator(aNumerator);
 		denominator = aDenominator;
+	}
+	
+	public int getDenominator() {
+		return denominator;
+	}
+	
+	public void setDenominator(int denom) {
+		if (denom != 0)	denominator = denom;
 	}
 	
 	public Fraction add(Fraction other) {
 		int newDenominator = denominator * other.denominator;
-		int newNumerator = numerator * other.denominator +
-			other.numerator * denominator;
+		int newNumerator = getNumerator() * other.denominator +
+			other.getNumerator() * denominator;
 		Fraction solution = new Fraction(newNumerator, newDenominator);
 		solution.simplify();
 		return solution;
 	}
 	
 	public void simplify() {
-		BigInteger num = BigInteger.valueOf(numerator);
+		BigInteger num = BigInteger.valueOf(getNumerator());
 		BigInteger den = BigInteger.valueOf(denominator);
 		BigInteger gcd = num.gcd(den);
-		numerator = numerator/gcd.intValue();
+		setNumerator(getNumerator()/gcd.intValue());
 		denominator = denominator/gcd.intValue();
 	}
 	
 	public boolean equals(Fraction other) {
-		return other.numerator == numerator && 
+		return other.getNumerator() == getNumerator() && 
 			other.denominator == denominator;
 	}
 	
 	public String toString(){
-		return numerator + "/" + denominator;
+		return getNumerator() + "/" + denominator;
+	}
+
+	public int getNumerator() {
+		return numerator;
+	}
+
+	public void setNumerator(int numerator) {
+		this.numerator = numerator;
 	}
 	
 }
